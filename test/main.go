8a88737f48f74bdf83b638fc520f7245b6dede21
Pikachu <savoninmv@gmail.com>
2024-09-03 12:25:08 +0500

    test

--- test/main.go
+++ test/main.go
@@ -9,12 +9,15 @@ import (
 //  "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func F(a map[int][]int){
-  a[1] = []int{10}
+func F(c chan int) {
+  var a int
+  fmt.Scan(&a)
+  c <- a
 }
 
 func main() {
-  var x map[int][]int = make(map[int][]int)
-  F(x)
-  fmt.Println(x[1])
+  var c chan int = make(chan int)
+  go F(c)
+  fmt.Println("Ждём ответ от другого потока")
+  fmt.Println("Получили ответ", <-c)
 }
